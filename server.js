694450a4fbd6baccc32af59b4672db11e6ba2e4e
DevI <adambeldi1703@gmail.com>
2025-07-29 15:29:47 +0200

    Update server.js
    Signed-off-by: DevI <adambeldi1703@gmail.com>

--- server.js
+++ server.js
@@ -1,22 +1,46 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
+const express = require('express');
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-io.on("connection", (socket) => {
-  console.log("Nouveau joueur connecté");
+const PORT = process.env.PORT || 3000;
 
-  socket.on("scoreUpdate", (data) => {
-    socket.broadcast.emit("scoreUpdate", data);
+let players = {};
+
+io.on('connection', (socket) => {
+  console.log('Un joueur connecté:', socket.id);
+
+  // Nouvel joueur
+  players[socket.id] = { score: 0, lives: 2 };
+
+  // Envoie l’état actuel aux autres joueurs
+  io.emit('players-update', players);
+
+  // Réception mise à jour joueur (score, vie)
+  socket.on('update-state', data => {
+    if(players[socket.id]) {
+      players[socket.id].score = data.score;
+      players[socket.id].lives = data.lives;
+      io.emit('players-update', players);
+    }
+  });
+
+  // Chat
+  socket.on('chat-message', msg => {
+    io.emit('chat-message', {id: socket.id, message: msg});
+  });
+
+  // Joueur déconnecté
+  socket.on('disconnect', () => {
+    console.log('Joueur déconnecté:', socket.id);
+    delete players[socket.id];
+    io.emit('players-update', players);
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+http.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
+
